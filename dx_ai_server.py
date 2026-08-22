f89b2d345f9cfc1e006b2389651c87d4b832df99
Monish K <monish.2022.k@gmail.com>
2026-08-22 13:18:53 +0530

    Restrict CORS to specific origin

--- dx_ai_server.py
+++ dx_ai_server.py
@@ -27,7 +27,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://zeta-dx.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
